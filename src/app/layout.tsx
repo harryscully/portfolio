8138b2a07f7f496e6e6eefa61f21d4818cdf91b3
harryscully <harry.scully@ugogroup.co.uk>
2026-02-27 23:22:17 +0000

    Add fun icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
+import consoleIcon from "../../public/console_prompt-0.png"
 import Navbar from "@/components/Navbar"
+import Image from "next/image"
 
 export default function RootLayout({
   children,
@@ -17,6 +19,7 @@ export default function RootLayout({
             >
               harryscully.com
             </a>
+            <Image src={consoleIcon} alt="console windows 98 icon" className="h-10 w-10"/>
             <Navbar />
           </div>
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import profilePic from "../../public/me.jpg"
+import folderIcon from "../../public/directory-closed-5.png"
 import Image from "next/image"
 
 const links = [
@@ -11,7 +12,8 @@ const links = [
 export default function Home() {
   const linkElements = links.map((link) => {
     return (
-      <li key={link.href}>
+      <li className="flex gap-4 items-center" key={link.href}>
+        <Image className="w-4 h-4" src={folderIcon} alt="folder windows 98 icon" />
         <a
           target="_blank"
           href={link.href}
@@ -32,7 +34,7 @@ export default function Home() {
           <ul className="flex gap-6">
             {linkElements}
           </ul>
-          <p>
+          <p className="leading-7">
             Hi, I'm Harry — a fullstack developer from Welwyn Garden City with a master's in Physics and Chemistry from Durham University, which I'm reliably informed is useful preparation for writing JavaScript. By day I build internal tools and technologies for Ugo Foods Group and Beyond Belief Brewing - a brewery that makes craft beer from manufacturing surplus, including fresh pasta, brownies and flapjacks (to name a few). By night (and most lunchtimes) I'm probably thinking about a quiz question. I compete in the Quiz League of London and the Online Quiz League, and I won University Challenge with Durham as the highest scoring individual in the <a target="_blank" className="font-semibold underline decoration-wavy hover:text-yellow-400 transition-all duration-200" href="https://www.youtube.com/watch?v=5zeIHCfC2Vk">final!</a> Outside of work and quizzing, you'll find me at the cinema with my girlfriend, suffering at the gym, or making slow but genuine progress through my ever-growing reading list.
           </p>
         </div>
