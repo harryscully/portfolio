81f87a517d4dd61f9d5b7755f8df49090917a4d0
harryscully <harry.scully@ugogroup.co.uk>
2026-02-27 23:47:28 +0000

    Fix image sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,18 +27,18 @@ export default function Home() {
   )
 
   return (
-    <div className="flex flex-col w-2/3 gap-8">
-      <h1>About</h1>
-      <div className="flex gap-12 justify-start">
-        <div className="flex flex-col gap-8 w-1/2">
-          <ul className="flex gap-6">
-            {linkElements}
-          </ul>
-          <p className="leading-7">
-            Hi, I'm Harry — a fullstack developer from Welwyn Garden City with a master's in Physics and Chemistry from Durham University, which I'm reliably informed is useful preparation for writing JavaScript. By day I build internal tools and technologies for Ugo Foods Group and Beyond Belief Brewing - a brewery that makes craft beer from manufacturing surplus, including fresh pasta, brownies and flapjacks (to name a few). By night (and most lunchtimes) I'm probably thinking about a quiz question. I compete in the Quiz League of London and the Online Quiz League, and I won University Challenge with Durham as the highest scoring individual in the <a target="_blank" className="font-semibold underline decoration-wavy hover:text-yellow-400 transition-all duration-200" href="https://www.youtube.com/watch?v=5zeIHCfC2Vk">final!</a> Outside of work and quizzing, you'll find me at the cinema with my girlfriend, suffering at the gym, or making slow but genuine progress through my ever-growing reading list.
-          </p>
-        </div>
-        <Image className="w-1/5" src={profilePic} alt="me" />
+    <div className="flex gap-16 justify-start">
+      <div className="flex flex-col w-130 gap-8">
+        <h1>About</h1>
+        <ul className="flex gap-6">
+          {linkElements}
+        </ul>
+        <p className="leading-7">
+          Hi, I'm Harry — a fullstack developer from Welwyn Garden City with a master's in Physics and Chemistry from Durham University, which I'm reliably informed is useful preparation for writing JavaScript. By day I build internal tools and technologies for Ugo Foods Group and Beyond Belief Brewing - a brewery that makes craft beer from manufacturing surplus, including fresh pasta, brownies and flapjacks (to name a few). By night (and most lunchtimes) I'm probably thinking about a quiz question. I compete in the Quiz League of London and the Online Quiz League, and I won University Challenge with Durham as the highest scoring individual in the <a target="_blank" className="font-semibold underline decoration-wavy hover:text-yellow-400 transition-all duration-200" href="https://www.youtube.com/watch?v=5zeIHCfC2Vk">final!</a> Outside of work and quizzing, you'll find me at the cinema with my girlfriend, suffering at the gym, or making slow but genuine progress through my ever-growing reading list.
+        </p>
+      </div>
+      <div className="w-60 my-20">
+        <Image src={profilePic} alt="me" />
       </div>
     </div>
   );
