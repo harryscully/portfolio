020fdc33c205e5dea0057f442ec0f7cbf560816a
harryscully <harry.scully@ugogroup.co.uk>
2026-02-28 15:42:56 +0000

    Change site title to a Link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import consoleIcon from "../../public/console_prompt-0.png"
 import Navbar from "@/components/Navbar"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function RootLayout({
   children,
@@ -13,12 +14,12 @@ export default function RootLayout({
       <body className="text-neutral-800">
         <div className="m-16 flex gap-16">
           <div className="flex flex-col gap-8 items-center">
-            <a 
+            <Link 
               className="tracking-widest"
               href="/"
             >
               harryscully.com
-            </a>
+            </Link>
             <Image src={consoleIcon} alt="console windows 98 icon" className="h-8 w-8"/>
             <Navbar />
           </div>
