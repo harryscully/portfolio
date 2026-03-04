8a7057b25b83c4cab3da50db3c860051f3f7e9b7
harryscully <harry.scully@ugogroup.co.uk>
2026-03-04 09:45:44 +0000

    Update hobbies page with bookIcon

--- src/app/hobbies/page.tsx
+++ src/app/hobbies/page.tsx
@@ -1,4 +1,5 @@
 import movieIcon from "../../../public/cd_drive_purple-0.png"
+import bookIcon from "../../../public/help-book-cool-3.png"
 import Image from "next/image"
 import Link from "next/link"
 import type { Metadata } from "next"
@@ -26,7 +27,7 @@ export default function HobbiesPage() {
                     className="flex items-center gap-4 p-4 border border-green-600 hover:bg-green-100 hover:text-green-600 transition-colors duration-200 cursor-pointer"
                     href="/hobbies/books"
                 >
-                    <Image className="h-8 w-8" src={movieIcon} alt="cd windows 98 icon" />
+                    <Image className="h-8 w-8" src={bookIcon} alt="cd windows 98 icon" />
                     <div>
                         <h2 className="font-semibold text-xl">Books</h2>
                         <p className="text-sm">Everything I've read, organised by year</p>
